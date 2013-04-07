8ffa2f2fd6fc7389c1dd49a1235ab2dea934b229
Abdullah Eid Almehmadi <cs.almehmadi@gmail.com>
2013-04-07 16:03:46 +0300

    "Clean up": example.php

--- example.php
+++ example.php
@@ -8,11 +8,6 @@ _f('users')->email('md5');
 $_('users');
 
 $result = $users->_('select','*');
-//$users->_('select_where_id')
-
-//$select('id,name')->from->users->where->id('=',10)->and->limit->asis('1,2')->
-
-//$smart->select('*')->from('student')->where->id('> 10')->and->name("!= 'salem'")
 
 $_->_('shutdown');
 
