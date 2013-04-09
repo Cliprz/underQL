35ffcdd4a6844724175ebf2377b47222739957d0
Abdullah Eid Almehmadi <cs.almehmadi@gmail.com>
2013-04-09 03:40:35 +0300

    example.php was modified

--- example.php
+++ example.php
@@ -1,15 +1,25 @@
 <?php ini_set('display_errors',1);
 
-require_once('underQL.php');
-
-
-_f('users')->email('md5');
+require_once('underQL.mini.php');
 
 $_('users');
 
-$result = $users->_('select','*');
+$users->name = 'Saleh';
+$users->password = 'lrkjq43';
+$users->email = 'a@b.c';
+
+$result = $users->_('insert');
+
+if(!$result && $users->_('are_rules_passed'))
+{
+	$e = $users->_('get_error_messages');
+	if($e->in('name'))
+	{
+		if($e->at('name','length'))
+		 die($e->get('name','length'));
+	}
+}
 
 $_->_('shutdown');
-
 
 ?>
