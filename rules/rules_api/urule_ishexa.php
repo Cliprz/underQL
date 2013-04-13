94901d2d06e6e9fe6b119796b148d660bc83b636
Abdullah Eid Almehmadi <cs.almehmadi@gmail.com>
2013-04-13 17:55:08 +0300

    urule_ishexa.php was modified

--- rules/rules_api/urule_ishexa.php
+++ rules/rules_api/urule_ishexa.php
@@ -3,7 +3,8 @@
 function urule_ishexa($name, $value, $alias = null, $params = null) {
 	if(!ctype_xdigit($value))
 		return "$value is not a proper hexadecimal value";
-		
+		//return UnderQL.rules.errors['ishexa'];
+		//UQLRoot.rules.errors.ishexa
 	return UQL_RULE_SUCCESS;
 }
 ?>
