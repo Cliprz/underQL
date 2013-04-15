eccfa956ab0e481c9ff9d53263af2af7a53e1d1d
Abdullah Eid Almehmadi <cs.almehmadi@gmail.com>
2013-04-16 02:30:29 +0300

    bug-fixed : at UQLRuleEngine

--- rules/rules_api/urule_length.php
+++ rules/rules_api/urule_length.php
@@ -2,13 +2,18 @@
 
 function urule_length($name, $value, $alias = null, $params = null) {
 	
+	//var_dump($params);
 	if($params == null)
 	 return 'يجب عليك تحديد الطول المطلوب قبل استخدامه مع الحقل '.$name;
 	
 	if(!is_int($params[0]) || $params[0] <= 0)
-	  return 'يجب عليك تحديد قيمة مناسبة للطول للحقل '.$name;
-	
-	if(mb_strlen($value) > $params[0])
+	{
+		if($alias != null)
+	 	    return 'يجب عليك تحديد قيمة مناسبة للطول لحقل '.$alias;
+		else 
+			return 'يجب عليك تحديد قيمة مناسبة للطول للحقل '.$name;
+	}
+	if(mb_strlen($value,'utf8') > $params[0])
 	  {
 	  	if($alias != null)
 		 return 'يجب أن لا يتجاوز طول '.$alias.' '.$params[0].' حرف/أحرف';
